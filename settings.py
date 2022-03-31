7a2e0abd2bbd867c4ea2a0b61d94582e40789814
gianmarcoturian <101284265+gianmarcoturian@users.noreply.github.com>
2022-03-31 18:14:50 +0200

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,40 @@
+from os import environ
+
+SESSION_CONFIGS = [
+
+
+dict(
+        name='experiment',
+        display_name='experiment',
+        app_sequence=['experiment'],
+        num_demo_participants=10,
+    ),
+]
+
+# if you set a property in SESSION_CONFIG_DEFAULTS, it will be inherited by all configs
+# in SESSION_CONFIGS, except those that explicitly override it.
+# the session config can be accessed from methods in your apps as self.session.config,
+# e.g. self.session.config['participation_fee']
+
+SESSION_CONFIG_DEFAULTS = dict(real_world_currency_per_point=0.005, participation_fee=0.00, doc="")
+
+# ISO-639 code
+# for example: de, fr, ja, ko, zh-hans
+LANGUAGE_CODE = 'en'
+
+# e.g. EUR, GBP, CNY, JPY
+REAL_WORLD_CURRENCY_CODE = 'GBP'
+USE_POINTS = True
+SESSION_FIELDS = ['prolific_completion_url']
+PARTICIPANT_FIELDS = ['finished']
+ADMIN_USERNAME = 'admin'
+# for security, best to set admin password in an environment variable
+ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+
+DEMO_PAGE_INTRO_HTML = """ """
+DEMO_PAGE_TITLE = "Experiment"
+
+SECRET_KEY = '4387860144726'
+
+# if an app is included in SESSION_CONFIGS, you don't need to list it here
+INSTALLED_APPS = ['otree']
